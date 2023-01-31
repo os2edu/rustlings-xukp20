c1ba831e6f513f78c2791fdb06759ee9e4508a46
xukangping <xukp20@mails.tsinghua.edu.cn>
2023-01-31 15:05:18 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(count: i32) -> i32 {
+    if count > 40 {
+        count
+    } else {
+        2 * count
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
